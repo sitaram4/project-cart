f7317f806f42e923df9b05e0024d312b2b5ab5dd
Ram <nagasitaram4@gmail.com>
2021-09-09 17:55:36 +0800

    obj destructuring and state initiliazation

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 class CartItem extends React.Component{
-    
+    constructor(){
+        super();
+        this.state = {
+            price:99,
+            title:'Mobile Phone',
+            qty:1,
+            img:''
+        }
+    }
     render(){
+        const {price,title,qty} = this.state;
         return (
             <div className = "cart-item">
                 <div className = "left-block">
                     <img style={style.image}/>
                 </div>
                 <div className="right-block">
-                    <div style ={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Price</div>
-                    <div style={{color:'#777'}}>Qty</div>
+                    <div style ={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>{price}</div>
+                    <div style={{color:'#777'}}>{qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons  */}
-
+                        <img alt="increase" className = "action-icons" src="https://image.flaticon.com/icons/png/512/1828/1828925.png"/>
+                        <img alt="decrease" className = "action-icons" src="https://image.flaticon.com/icons/png/512/860/860821.png"/>
+                        <img alt="delete" className = "action-icons" src="https://image.flaticon.com/icons/png/512/3096/3096687.png"/>
                     </div>
                 </div>
                 
